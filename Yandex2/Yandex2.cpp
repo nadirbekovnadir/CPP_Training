7b9f5ec1a3c30a72f34ba02e3c927e2bbc369b88
Надир Надирбеков <nadirbekovnadir@gmail.com>
2020-12-31 17:47:52 +0300

    Yandex tasks added

---
+++ Yandex2/Yandex2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    string input;
+    int n, m;
+
+    getline(cin, input);
+
+    size_t pos = input.find(' ');
+    size_t initialPos = 0;
+
+    n = atoi(input.substr(0, pos).c_str());
+    m = atoi(input.substr(pos, input.length()).c_str());
+
+    vector<int> b;
+    b.resize(n);
+
+    for (int i = 0; i < n; ++i)
+        cin >> b[i];
+
+    unordered_set<int> forFind;
+    list<int> result;
+
+    int index = -1;
+    int type = -1;
+    bool crack = false;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (forFind.find(i) != forFind.end())
+            continue;
+
+        if (b[i] != type)
+        {
+            type = b[i];
+            forFind.insert(i);
+            cout << i << ' ';
+
+            if (crack)
+            {
+                i = index - 1;
+                crack = false;
+            }
+
+            continue;
+        }
+
+        if (crack == false)
+        {
+            index = i;
+            crack = true;
+        }
+    }
+
+}
---
+++ Yandex4/Yandex4.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <cstdlib>
+#include <ctime>
+
+uint64_t get_hash(const std::string& s, int table_size);
+
+
+const int tableSize = 280000;
+const int strSize = 1000;
+
+uint32_t keys[strSize];
+uint32_t init_h;
+
+int main()
+{
+	srand(time(0));
+
+	for (int i = 0; i < strSize; ++i)
+	{
+		keys[i] = rand();
+		keys[i] = keys[i] == 0 ? 1 : keys[i];
+	}
+
+	init_h = rand();
+	init_h = init_h == 0 ? 1 : init_h;
+
+	std::string val;
+	val.reserve(strSize);
+
+	int n = 0;
+	std::getline(std::cin, val);
+	n = atoi(val.substr(0, val.size()).c_str());
+
+	uint32_t hashP = 0;
+
+	uint8_t* hash = new uint8_t[tableSize];
+	for (int i = 0; i < tableSize; ++i)
+		hash[i] = 0;
+
+	int k = 0;
+	int offset = 0;
+	int index = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		std::getline(std::cin, val);
+
+		hashP = get_hash(val, tableSize * 8 + 1);
+
+		index = hashP / 8;
+		offset = hashP % 8;
+		if ((hash[index] & (1 << offset)) == 0)
+		{
+			++k;
+			hash[index] |= 1 << offset;
+		}
+	}
+
+	std::cout << k;
+}
+
+uint64_t get_hash(const std::string& s, int table_size)
+{
+	uint64_t hash_result = init_h;
+
+	for (int i = 0; i < s.size(); ++i)
+		hash_result = (keys[i] * hash_result + (uint8_t)s[i]) % table_size;
+
+	return (hash_result <= 0) ? (hash_result + table_size) : hash_result;
+}
